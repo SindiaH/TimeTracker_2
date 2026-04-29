7e2060d0b320e6986bf8cb6de81838fc5b3dc9d9
sindia.hoeller <sindia.hoeller@outlook.com>
2026-04-29 20:33:56 +0200

    Angular Material installieren und Theme-Skelett

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,12 +1,14 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialComponentsModule } from '@shared/material-components/material-components.module';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
 @NgModule({
   declarations: [App],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialComponentsModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
---
+++ src/app/shared/material-components/material-components.module.ts
@@ -0,0 +1,119 @@
+import { NgModule } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import {
+  MatAnchor,
+  MatButton,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton,
+} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTab, MatTabGroup, MatTabLabel, MatTabLink, MatTabNav, MatTabNavPanel } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltip, MatTooltipModule } from '@angular/material/tooltip';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormField } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatBadge } from '@angular/material/badge';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatToolbar,
+    MatButton,
+    MatIconModule,
+    MatIconButton,
+    MatAnchor,
+    MatMiniFabButton,
+    MatIconAnchor,
+    MatMiniFabAnchor,
+    MatFabButton,
+    MatFabAnchor,
+    MatTabGroup,
+    MatTab,
+    MatTabLabel,
+    MatTabNav,
+    MatTabLink,
+    MatTabNavPanel,
+    MatCardModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatChipsModule,
+    MatFormField,
+    MatDatepickerModule,
+    MatInputModule,
+    MatBadge,
+    MatDialogModule,
+    MatDividerModule,
+    MatListModule,
+    MatExpansionModule,
+    MatMenuModule,
+    MatProgressBar,
+    MatProgressSpinner,
+    MatSidenavModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatButtonToggleModule,
+  ],
+  exports: [
+    MatTooltip,
+    MatToolbar,
+    MatButton,
+    MatIconModule,
+    MatIconButton,
+    MatAnchor,
+    MatMiniFabButton,
+    MatIconAnchor,
+    MatMiniFabAnchor,
+    MatFabButton,
+    MatFabAnchor,
+    MatTabGroup,
+    MatTab,
+    MatTabLabel,
+    MatTabNav,
+    MatTabLink,
+    MatTabNavPanel,
+    MatCardModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatChipsModule,
+    MatFormField,
+    MatDatepickerModule,
+    MatInputModule,
+    MatBadge,
+    MatDialogModule,
+    MatDividerModule,
+    MatListModule,
+    MatExpansionModule,
+    MatMenuModule,
+    MatProgressBar,
+    MatProgressSpinner,
+    MatSidenavModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatButtonToggleModule,
+  ],
+})
+export class MaterialComponentsModule {}
